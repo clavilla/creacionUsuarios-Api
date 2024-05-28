6d8952b813f918ef47820885fa0d0a6c3c9b713e
clavilla <cristianlavilla@hotmail.com>
2024-05-28 09:44:43 -0300

    feature: se modificóoversiÃon de spring boot por vulnerabilidades

--- src/main/java/com/clavilla/userapi/UserApiApplication.java
+++ src/main/java/com/clavilla/userapi/UserApiApplication.java
@@ -1,4 +1,4 @@
-package com.clavilla.user_Api;
+package com.clavilla.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/clavilla/userapi/UserApiApplicationTests.java
+++ src/test/java/com/clavilla/userapi/UserApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.clavilla.user_Api;
+package com.clavilla.userapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
